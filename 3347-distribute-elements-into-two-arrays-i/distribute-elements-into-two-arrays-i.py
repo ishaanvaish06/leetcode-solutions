0b706c8acea93c031126089a276213d094e04bd3
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-08-20 09:41:28 +0530

    Time: 11 ms (1.92%) | Memory: 19.4 MB (22.76%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        l1=list()
+        l2=list()
+        l1.append(nums[0])
+        l2.append(nums[1])
+
+        a=nums[0]
+        b=nums[1]
+
+        for i in range(2,len(nums)):
+            if a>b:
+                l1.append(nums[i])
+                a=nums[i]
+                print(l1)
+            else:
+                l2.append(nums[i])
+                b=nums[i]
+                print(l2)
+
+        return l1+l2
+        
